e0cd69e183d1d7ebb40e141c3cccedc97331ea1c
siphosetu mzayiya <siphosetumzayiya@siphosetus-MacBook-Air.local>
2024-03-06 15:52:04 +0200

    Initial commit

---
+++ string_num_input.py
@@ -0,0 +1,16 @@
+
+my_name = "Siphosetu"
+my_surname = "Mzayiya"
+glory_years = 1000
+hero_country = "South Africa"
+y = 1000
+x = 100
+t = y * x
+
+print("This is a story of " + my_name + my_surname + " the great rise of a hero")
+print("The hero was from " + hero_country)
+print("He rose to conquer the world")
+print("He rewrote history for the next " + str(glory_years) + "years")
+print("His name was " + my_name + my_surname)
+
+print(t)
